a1f69b049bbb3a1f5c378fe31d63337a94819025
Alex Karpov <aezred2@yandex.ru>
2022-08-24 01:54:31 +0300

    patch user

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { centralErrorsHandler } = require('./middlewares/centralErrorsHandler');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const options = {
   origin: [
